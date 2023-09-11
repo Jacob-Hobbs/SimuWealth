24e2a64a8b789d58250c730dceae71dd9b57855a
Jacob Hobbs <JacobRayHobbs@gmail.com>
2023-09-11 17:37:29 -0500

    First commit

---
+++ src/main/java/com/projects/simuwealth/simuwealth/SimuWealthApplication.java
@@ -0,0 +1,13 @@
+package com.projects.simuwealth.simuwealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimuWealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimuWealthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projects/simuwealth/simuwealth/SimuWealthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.simuwealth.simuwealth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimuWealthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
