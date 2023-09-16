2d453048d2dfc1a8f4b1daf7f062a5d05c54fa9e
Jacob Hobbs <JacobRayHobbs@gmail.com>
2023-09-15 21:03:33 -0500

    Third commit of 09/15

--- src/main/java/com/projects/simuwealth/simuwealth/Controller/LoginController.java
+++ src/main/java/com/projects/simuwealth/simuwealth/Controller/LoginController.java
@@ -2,12 +2,15 @@ package com.projects.simuwealth.simuwealth.Controller;
 
 import com.projects.simuwealth.simuwealth.Entity.User;
 import com.projects.simuwealth.simuwealth.Repository.UserRepository;
+import com.projects.simuwealth.simuwealth.Service.MailService.EmailSenderService;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/")
@@ -15,6 +18,9 @@ public class LoginController {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private EmailSenderService emailSenderService;
 
     @PostConstruct
     public void loadSampleData() {
@@ -24,7 +30,21 @@ public class LoginController {
         defaultAdmin.setLastName("Hobbs");
         defaultAdmin.setPassword("password");
 
+        User testAdmin = new User();
+        testAdmin.setEmail("jrh0397@gmail.com");
+        testAdmin.setFirstName("Jacob");
+        testAdmin.setLastName("Hobbs");
+        testAdmin.setPassword("password");
+
+        User jackiAdmin = new User();
+        jackiAdmin.setEmail("jackimcgoon@gmail.com");
+        jackiAdmin.setFirstName("Jacki");
+        jackiAdmin.setLastName("Hobbs");
+        jackiAdmin.setPassword("password");
+
         userRepository.save(defaultAdmin);
+        userRepository.save(testAdmin);
+        userRepository.save(jackiAdmin);
     }
 
     @GetMapping("/login")
@@ -39,7 +59,36 @@ public class LoginController {
     public String forgotPassword(Model model) {
         User user = new User();
         model.addAttribute("user", user);
-                return "forgotPassword";
+        return "forgotPassword";
+    }
+
+    @GetMapping("/resetPassword")
+    public String resetPassword(Model model) {
+        User user = new User();
+        model.addAttribute("user", user);
+        model.addAttribute("error", false);
+        return "resetPassword";
+    }
+
+    @PostMapping("/resetPassword")
+    public String resetPassword(@RequestParam("email") String email, Model model) {
+
+        User user = userRepository.findByEmail(email);
+
+        if (user == null) {
+            model.addAttribute("error", true);
+            return "resetPassword";
+        } else {
+
+            String subject = "Password Reset";
+            String body = "Please follow the following link to reset your email: http://localhost:8080/resetPassword";
+
+            emailSenderService.sendEmail(email, subject, body);
+
+        }
+        return "redirect:/login";
+
+
     }
 
 
--- src/main/java/com/projects/simuwealth/simuwealth/SimuWealthApplication.java
+++ src/main/java/com/projects/simuwealth/simuwealth/SimuWealthApplication.java
@@ -1,17 +1,28 @@
 package com.projects.simuwealth.simuwealth;
 
+import com.projects.simuwealth.simuwealth.Service.MailService.EmailSenderService;
 import com.projects.simuwealth.simuwealth.Service.StockService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class SimuWealthApplication {
+
+	//@Autowired
+	//private EmailSenderService senderService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SimuWealthApplication.class, args);
 
 	}
+
+	//@EventListener(ApplicationReadyEvent.class)
+	//public void sendMail() {
+		//senderService.sendEmail("jrh0397@gmail.com", "Reset Password Link", "Please reset your password here: ");
+	//}
 
 
 }
